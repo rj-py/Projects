8070ca359cfd42d5ef36e8e9453d5c302985fb12
rj-py <rhaaheemjackson@gmail.com>
2017-07-08 15:06:38 -0400

    Nothing special, just a python file, thats all

---
+++ Razzy.py
@@ -0,0 +1,8 @@
+name=input("Whats your name?: ")
+input=input("Do you have an email y/n?: ")
+if input=="y":
+    email=input("Please type your email?: ")
+    print("OK your email is",email)
+if input =="n":
+    print("Ok have a good day",name)
+
